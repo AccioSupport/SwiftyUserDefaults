9732abec445bb37eaf682155bf614a6bc9a18bd9
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2019-04-08 13:21:31 +0200

    Rename Package_5.0.swift to Package@swift-5.0.swift

--- Package@swift-5.0.swift
+++ Package@swift-5.0.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftyUserDefaults",
-    platforms: [.iOS("8.0"), .macOS("10.11"), tvOS("9.0"), .watchOS("2.0")],
+    platforms: [.iOS("8.0"), .macOS("10.11"), .tvOS("9.0"), .watchOS("2.0")],
     products: [
         .library(
             name: "SwiftyUserDefaults",
